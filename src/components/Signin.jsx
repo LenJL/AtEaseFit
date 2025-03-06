62cc4760f3b60c3418ea46aa929a3ada96e07ed8
kiruba-sam <kirubasamuel23339@gmail.com>
2025-03-06 13:07:55 +0530

    Update Signin.jsx

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -74,12 +74,7 @@ export default function Signin() {
               </div>
             </form>
   
-            <p className="mt-10 text-center text-sm/6 text-gray-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
-              </a>
-            </p>
+         
           </div>
         </div>
         </div>
